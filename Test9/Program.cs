1660c2f11c382e1ffa91cd5ff2b40f9c2767abb3
Nele Oks <nele.oks@tktk.ee>
2024-12-12 20:25:38 +0200

    add Week9Lists code

---
+++ Test9/Program.cs
@@ -0,0 +1,32 @@
+﻿//List<string> myToDo = new List<string>();
+//myToDo.Add("walk the dog");
+//myToDo.Add("do the dishes");
+//myToDo.Add("bake a pie");
+
+//using System.Collections.Generic;
+
+///------------------------------------------
+///
+//string folderPath = @"C:\Users\rait\Documents\data";
+//string fileName = "shoppingList.txt";
+
+//if (File.Exists(Path.Combine(folderPath, fileName)))
+//{
+//    Console.WriteLine($"File {fileName} exists.");
+
+//}
+//else
+//{
+//    Console.WriteLine($"File {fileName} does not exist.");
+//}
+
+
+//---------------------------------------------------------
+
+static void DisplayDataFromFile(List<string> someList)
+{
+    foreach (string line in someList)
+    {
+        Console.WriteLine(line);
+    }
+}
